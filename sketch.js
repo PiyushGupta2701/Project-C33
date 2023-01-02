b91aa436bc23c73a6337155cd70206b030eff99e
PiyushGupta2701 <89253611+PiyushGupta2701@users.noreply.github.com>
2023-01-02 12:44:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var bg, bgImg;
+var snow, snowImg;
+
+function preload(){
+  bgImg = loadImage("snow1.jpg");
+  snowImg = loadImage("snow4.webp");
+}
+
+function setup() {
+  createCanvas(1000,1000);
+  bg = createSprite(500, 500, 1000, 10);
+  bg.addImage(bgImg);
+  bg.scale = 2;
+}
+
+function draw() {
+  background(255,255,255);
+  spawnSnow();  
+  drawSprites();
+}
+
+function spawnSnow(){
+  if(frameCount % 40 === 0 ){
+    snow = createSprite(random(10,1000), 0, 10, 10);
+    snow.velocityY = 3;
+    snow.addImage(snowImg);
+    snow.lifetime = 300;
+    snow.scale = 0.1;
+  }
+}
